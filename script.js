e13c094bf174361ab2f40be7582ffdfd7eca6033
thouficgit97 <f.thouficyousuf0987@gmail.com>
2024-04-12 19:35:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var barIcon = document.querySelector(".navbar-toggle");
+var sideNav = document.getElementById("side-navbar");
+function showNavbar(){
+    sideNav.style.right="0";
+}
+function hideNavbar(){
+    sideNav.style.right="-60%"
+}
