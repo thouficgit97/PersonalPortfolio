fc095441149a4c89155e1320a31f8743f9cdeb0c
thouficgit97 <f.thouficyousuf0987@gmail.com>
2024-04-14 09:51:04 +0530

    fifth commit

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var barIcon = document.querySelector(".navbar-toggle");
 var sideNav = document.getElementById("side-navbar");
-function showNavbar(){
-    sideNav.style.right="0";
+var mode = document.getElementById("mode");
+function showNavbar() {
+  sideNav.style.right = "0";
 }
-function hideNavbar(){
-    sideNav.style.right="-60%"
+function hideNavbar() {
+  sideNav.style.right = "-60%";
+}
+function hidesideNav() {
+  sideNav.style.right = "-60%";
 }
